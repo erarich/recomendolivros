0889918d93f81bc3b308da5f1ce82498fa1bb068
Érick Santos <ericksantos@mailfence.com>
2024-01-16 18:10:17 -0300

    adds functions.py file

--- Home.py
+++ Home.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from src.functions import load_data
+
 
 st.set_page_config(page_title='Recomendador de Livros', page_icon='📖')
 
